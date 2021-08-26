20ba599b5a113428a3d3caabf0db1bc719b04413
alhiane999 <alhiane.hero1998@gmail.com>
2021-08-26 18:35:09 +0100

    Gradient Background generator Added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const inputClr = document.querySelectorAll('.input-clr');
+const gradientValue = document.getElementById('gradient-value');
+
+// init linear gradient:
+inputClr[0].value = '#9effae';
+inputClr[1].value = '#2acba3';
+var clr1 = inputClr[0].value, clr2 = inputClr[1].value;
+document.body.style.backgroundImage = `linear-gradient( to right, ${clr1}, ${clr2})`;
+
+// change linear gradient:
+inputClr.forEach(inpt => {
+    inpt.addEventListener('input', _ => {
+        if (inpt.classList.contains('inpt-clr-1')) clr1 = inpt.value;
+        else clr2 = inpt.value;
+        document.body.style.backgroundImage = `linear-gradient( to right, ${clr1}, ${clr2})`;
+        let red1 = parseInt(clr1.substr(1, 2), 16);
+        let red2 = parseInt(clr2.substr(1, 2), 16);
+        let green1 = parseInt(clr1.substr(3, 2), 16);
+        let green2 = parseInt(clr2.substr(3, 2), 16);
+        let blue1 = parseInt(clr1.substr(5, 2), 16);
+        let blue2 = parseInt(clr2.substr(5, 2), 16);
+        gradientValue.innerHTML = `linear-gradient( to right, 
+                                   rgb(${red1}, ${green1}, ${blue1}), 
+                                   rgb(${red2}, ${green2}, ${blue2}))`;
+    });
+});
